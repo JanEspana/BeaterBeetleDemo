0befeb65152d698c828ebff039ac27f49bc925b7
David Galan <david.galan.7e7@itb.cat>
2025-02-12 17:46:42 +0100

    added timer and food spawning script

--- Assets/Scripts/FoodRound/FoodRoundManager.cs
+++ Assets/Scripts/FoodRound/FoodRoundManager.cs
@@ -1,32 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class FoodRoundManager : MonoBehaviour
 {
     //timer
     public float timeBetweenEggs = 3f;
-    public float totalTimer = 30f;
+    public float totalTimer = 10f;
+    public float timer;
     public GameObject eggPrefab;
+    public GameObject foodPrefab;
+    public GameObject roundTimer;
     public void Start()
     {
         StartFoodRound();
     }
     public void StartFoodRound()
     {
-        StartCoroutine(FoodRound());
+        Instantiate(foodPrefab, new Vector3(5, 20, 0), Quaternion.identity);
+        StartCoroutine(EggSpawn());
+        StartCoroutine(RoundTimer());
     }
-    IEnumerator FoodRound()
+    IEnumerator EggSpawn()
     {
-        float timer = 0;
         while (timer < totalTimer)
         {
-            timer += Time.deltaTime;
             yield return new WaitForSeconds(timeBetweenEggs);
             //instatiate egg in random position
             Vector3 randomPos = new Vector3(Random.Range(-20, 20), 2, Random.Range(-20, 20));
             Instantiate(eggPrefab, randomPos, Quaternion.identity);
         }
         Debug.Log("Food Round Over");
+    }
+    IEnumerator RoundTimer()
+    {
+        timer = 0;
+        while (timer < totalTimer)
+        {
+            timer += Time.deltaTime;
+            roundTimer.GetComponent<TextMeshProUGUI>().text = (totalTimer - timer).ToString("F0");
+            yield return null;
+        }
     }
 }
--- Assets/Scripts/FoodRound/FoodScript.cs
+++ Assets/Scripts/FoodRound/FoodScript.cs
@@ -1,21 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FoodScript : Character
 {
+    public Slider healthBarSlider;
+    private void Awake()
+    {
+        healthBarSlider = GameObject.FindGameObjectWithTag("Slider").GetComponent<Slider>();
+        healthBarSlider.maxValue = HP;
+        healthBarSlider.value = HP;
+    }
     public override void CheckIfAlive(bool hasKnockback)
     {
+        healthBarSlider.value = HP;
         if (HP <= 0)
         {
             //busca todos los objetos con tag enemy y cambia su target al objeto con tag player
             GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
             foreach (GameObject enemy in enemies)
             {
-                enemy.GetComponent<EnemyController>().target = GameObject.FindGameObjectWithTag("Player");
-                enemy.GetComponent<EnemyController>().foodIsAlive = false;
+                if (enemy.GetComponent<EnemyController>() != null)
+                {
+                    enemy.GetComponent<EnemyController>().target = GameObject.FindGameObjectWithTag("Player");
+                    enemy.GetComponent<EnemyController>().foodIsAlive = false;
+                }
+
             }
-            Die();
+            Destroy(gameObject);
         }
     }
 
